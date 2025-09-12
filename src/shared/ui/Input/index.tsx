7a5c08d1d7b13f648a49b64acfbc30b1b5a6b06b
bae080311 <s24024@gsm.hs.kr>
2025-09-12 11:51:36 +0900

    feat: input component

---
+++ src/shared/ui/Input/index.tsx
@@ -0,0 +1,22 @@
+interface InputProps {
+  label: string;
+}
+
+export default function Input({
+  label,
+  ...props
+}: InputProps & React.InputHTMLAttributes<HTMLInputElement>) {
+  return (
+    <div className="flex flex-col gap-1">
+      <label className="body-4" htmlFor={label}>
+        {label}
+      </label>
+      <input
+        className="rounded-lg border border-gray-500 px-5 py-4"
+        {...props}
+        id={label}
+        type="text"
+      />
+    </div>
+  );
+}
