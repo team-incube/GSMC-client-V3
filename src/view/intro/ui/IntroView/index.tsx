225bda4688f795cec067741bff16b3fc0760bb00
bae080311 <s24024@gsm.hs.kr>
2025-09-12 11:25:14 +0900

    feat: intro view

---
+++ src/view/intro/ui/IntroView/index.tsx
@@ -0,0 +1,19 @@
+import Image from 'next/image';
+import LogoImage from '@/shared/asset/img/logo.png';
+import GoogleLogo from '@/shared/asset/svg/GoogleLogo';
+import Button from '@/shared/ui/Button';
+
+export default function IntroView() {
+  return (
+    <div className="flex h-screen flex-col items-center justify-around">
+      <div />
+      <Image alt="GSMC logo" height={408} src={LogoImage} width={408} />
+      <div className="w-full max-w-[600px]">
+        <Button variant="disabled_border">
+          <GoogleLogo />
+          <span className="ml-6">Google로 시작하기</span>
+        </Button>
+      </div>
+    </div>
+  );
+}
