265387f12a7c7d0ee211e21497f1b3168f1e72c3
bae080311 <s24024@gsm.hs.kr>
2025-10-03 21:27:04 +0900

    fix: check cookie

--- src/shared/lib/cookie.ts
+++ src/shared/lib/cookie.ts
@@ -1,8 +1,10 @@
 const setCookie = (key: string, value: string) => {
+  if (typeof document === 'undefined') return;
   document.cookie = `${encodeURIComponent(key)}=${encodeURIComponent(value)}; path=/`;
 };
 
 const getCookie = (key: string) => {
+  if (typeof document === 'undefined') return null;
   const name = encodeURIComponent(key) + '=';
   const cookies = document.cookie.split(';');
   for (let cookie of cookies) {
@@ -15,6 +17,7 @@ const getCookie = (key: string) => {
 };
 
 const deleteCookie = (key: string) => {
+  if (typeof document === 'undefined') return;
   document.cookie = `${encodeURIComponent(key)}=; path=/; expires=Thu, 01 Jan 1970 00:00:00 GMT`;
 };
 
