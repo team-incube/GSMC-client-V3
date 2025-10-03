7a0633ccd19e7e7039f038a2ced5ce33ad761a87
bae080311 <s24024@gsm.hs.kr>
2025-10-03 20:05:15 +0900

    feat: signup view

---
+++ src/view/signup/ui/SignupView/index.tsx
@@ -0,0 +1,16 @@
+import BackArrow from '@/shared/asset/svg/BackArrow';
+import SignupForm from '@/widget/signup/ui/SignupForm';
+
+export default function SignupView() {
+  return (
+    <div className="flex justify-center">
+      <div className="flex w-full max-w-[600px] flex-col gap-3">
+        <header className="flex items-center">
+          <BackArrow />
+          <h2 className="text-main-700 text-titleSmall">내 정보 등록</h2>
+        </header>
+        <SignupForm />
+      </div>
+    </div>
+  );
+}
