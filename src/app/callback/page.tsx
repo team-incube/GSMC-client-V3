ea8f0af25b02749882636a8afbf53c1874c5cf96
bae080311 <s24024@gsm.hs.kr>
2025-10-05 00:40:20 +0900

    fix: build error

--- src/app/callback/page.tsx
+++ src/app/callback/page.tsx
@@ -1,5 +1,12 @@
 import CallbackView from '@/view/callback/ui/CallbackView';
+import { Suspense } from 'react';
+
+export const dynamic = 'force-dynamic';
 
 export default function CallbackPage() {
-  return <CallbackView />;
+  return (
+    <Suspense fallback={null}>
+      <CallbackView />
+    </Suspense>
+  );
 }
--- src/view/callback/ui/CallbackView/index.tsx
+++ src/view/callback/ui/CallbackView/index.tsx
@@ -5,17 +5,27 @@ import { useSearchParams } from 'next/navigation';
 
 export default function CallbackView() {
   const searchParams = useSearchParams();
+
   const code = searchParams.get('code');
 
   useEffect(() => {
-    if (code) {
-      fetch('/auth', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ code }),
-      });
+    async function run() {
+      try {
+        if (code) {
+          await fetch('/api/auth/google/callback', {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify({ code }),
+            credentials: 'include',
+          });
+        }
+      } catch (e) {
+        throw e;
+      }
     }
+
+    run();
   }, [code]);
 
-  return <div>로그인 중...</div>;
+  return null;
 }
