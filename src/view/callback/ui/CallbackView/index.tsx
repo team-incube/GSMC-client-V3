82d20c7ce5b344ddb2e918c59604d964178805c6
bae080311 <s24024@gsm.hs.kr>
2025-10-05 00:56:38 +0900

    remove: test

--- src/view/callback/ui/CallbackView/index.tsx
+++ src/view/callback/ui/CallbackView/index.tsx
@@ -9,7 +9,6 @@ export default function CallbackView() {
   const code = searchParams.get('code');
 
   useEffect(() => {
-    console.log('test');
     async function run() {
       try {
         if (code) {
