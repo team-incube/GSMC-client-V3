19b3bd76e989a7a28c13cfaf6012126db2144107
bae080311 <s24024@gsm.hs.kr>
2025-10-05 00:28:32 +0900

    fix: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -60,6 +60,6 @@ export function middleware(request: NextRequest) {
 export const config = {
   matcher: [
     '/api/:path*',
-    '/((?!_next/static|_next/image|favicon.ico|.*\.(png|jpg|jpeg|gif|svg)).*)',
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:png|jpg|jpeg|gif|svg|webp|ico)).*)',
   ],
 };
