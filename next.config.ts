aacbfb4b0bbd2017f69810e0adfefe3b011ce6c4
Dino0204 <s24020@gsm.hs.kr>
2025-11-02 11:43:54 +0900

    chore: update react compiler version

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: __dirname,
-  experimental: {
-    reactCompiler: true,
-  },
+  reactCompiler: true,
+  
 };
 
 export default nextConfig;
