b68b90605d15f4b806e5cf934b482435157bd570
moonkanghyeon <s25024@gsm.hs.kr>
2025-11-05 20:11:58 +0900

    feat: ChevronDown SVG에 크기/색상 props 추가

--- src/shared/asset/svg/ChevronDown.tsx
+++ src/shared/asset/svg/ChevronDown.tsx
@@ -1,7 +1,24 @@
-export default function ChevronDown() {
+interface ChevronDownProps {
+  width?: number;
+  height?: number;
+  color?: string;
+  className?: string;
+}
+
+export default function ChevronDown({
+  width = 32,
+  height = 32,
+  color = '#385B97',
+}: ChevronDownProps) {
   return (
-    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M24 13.3335L16 21.3335L8 13.3335" stroke="#385B97" strokeLinecap="round" />
+    <svg
+      width={width}
+      height={height}
+      viewBox="0 0 32 32"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path d="M24 13.3335L16 21.3335L8 13.3335" stroke={color} strokeLinecap="round" />
     </svg>
   );
 }
