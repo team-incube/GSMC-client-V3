1bade2e5f2f918dc9acfab64a571717aa2e928b4
bae080311 <s24024@gsm.hs.kr>
2025-11-05 14:29:13 +0900

    fix: font size

--- src/shared/ui/Input/index.tsx
+++ src/shared/ui/Input/index.tsx
@@ -8,7 +8,7 @@ export default function Input({
 }: InputProps & React.InputHTMLAttributes<HTMLInputElement>) {
   return (
     <div className="flex flex-col gap-1">
-      <label className="body-4 text-main-700" htmlFor={label}>
+      <label className="text-body1 text-main-700" htmlFor={label}>
         {label}
       </label>
       <input className="rounded-2xl border border-gray-500 p-4" {...props} id={label} type="text" />
