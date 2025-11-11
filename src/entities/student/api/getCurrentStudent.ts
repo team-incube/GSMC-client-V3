7081b33410556e9b805aca8e4b62d044dced90a6
Dino0204 <s24020@gsm.hs.kr>
2025-11-11 16:04:27 +0900

    docs: 학생 -> 사용자, 로그인한 -> 인증된

--- src/entities/student/api/getCurrentStudent.ts
+++ src/entities/student/api/getCurrentStudent.ts
@@ -2,7 +2,7 @@ import { instance } from '@/shared/lib/axios';
 import type { StudentType } from '../model/StudentSchema';
 
 /**
- * 현재 로그인한 학생 정보 조회
+ * 현재 인증된 사용자 정보 조회
  * @Todo MainView에서 사용 예정
  * @status UNUSED - 구현됨, 아직 사용되지 않음
  */
