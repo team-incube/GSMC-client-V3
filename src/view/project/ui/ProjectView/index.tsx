9bab594e4f89fb255e915290b1be4aa8eb4d55c5
bae080311 <s24024@gsm.hs.kr>
2025-11-11 12:12:51 +0900

    fix: view layout

--- src/view/project/ui/ProjectView/index.tsx
+++ src/view/project/ui/ProjectView/index.tsx
@@ -3,10 +3,10 @@ import ProjectForm from '@/widget/project/ui/ProjectForm';
 
 export default function ProjectView() {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col items-center px-6">
       <Header />
       <section className="w-full max-w-[600px]">
-        <h1 className="text-main-700 text-titleMedium">프로젝트 참여</h1>
+        <h1 className="text-main-700 text-titleMedium my-9">프로젝트 참여</h1>
         <ProjectForm />
       </section>
     </div>
