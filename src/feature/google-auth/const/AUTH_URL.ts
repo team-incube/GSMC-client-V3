505872e01f58af014d7e795a825b0101e74e1d20
Dino0204 <s24020@gsm.hs.kr>
2025-11-14 10:28:05 +0900

    feat: GOOGLE_URL 가독성 향상을 위한 포맷 변경

--- src/feature/google-auth/const/AUTH_URL.ts
+++ src/feature/google-auth/const/AUTH_URL.ts
@@ -1,2 +1,16 @@
-export const GOOGLE_SIGNUP_URL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20email&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_SIGNUP_REDIRECT_URI}`;
-export const GOOGLE_SIGNIN_URL = `https://accounts.google.com/o/oauth2/v2/auth?response_type=code&scope=openid%20email&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_SIGNIN_REDIRECT_URI}&prompt=consent`;
+export const GOOGLE_SIGNUP_URL = `
+https://accounts.google.com/o/oauth2/v2/auth?
+response_type=code
+&scope=openid%20email
+&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_SIGNUP_REDIRECT_URI}
+`;
+
+export const GOOGLE_SIGNIN_URL = `
+https://accounts.google.com/o/oauth2/v2/auth?
+response_type=code
+&scope=openid%20email
+&client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_SIGNIN_REDIRECT_URI}
+&prompt=consent
+`;
--- src/feature/google-auth/lib/handleGoogleButton.ts
+++ src/feature/google-auth/lib/handleGoogleButton.ts
@@ -1,4 +1,9 @@
-const GOOGLE_URL = `https://accounts.google.com/o/oauth2/v2/auth?client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}&response_type=code&scope=email profile`;
+const GOOGLE_URL = `
+https://accounts.google.com/o/oauth2/v2/auth?
+client_id=${process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID}
+&redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_REDIRECT_URI}
+&response_type=code
+&scope=email+profile`;
 
 export const handleGoogleButton = () => {
   window.location.href = GOOGLE_URL;
