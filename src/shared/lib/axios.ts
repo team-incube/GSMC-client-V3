e71b66d51c8459eafb6597cf2546520d0c62eeb0
Dino0204 <s24020@gsm.hs.kr>
2025-11-15 19:38:43 +0900

    docs: Axios 인터셉터 주석 추가

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -17,18 +17,21 @@ instance.interceptors.response.use(
     return response;
   },
   async (error: AxiosError) => {
+    
     const originalRequest = error.config as InternalAxiosRequestConfig & { _retry?: boolean };
 
     if (error.response?.status === 401 && !originalRequest._retry) {
       originalRequest._retry = true;
 
       try {
+        // Route Handler API를 호출하여 토큰 갱신 시도
         await axios.post('http://localhost:3000/api/auth/refresh',{},{withCredentials: true});
         
+        // 토큰 갱신이 성공하면 원래 요청을 다시 시도
         return instance(originalRequest);
 
       } catch (error) {
-        
+        // 토큰 갱신 실패 시 로그인 페이지로 리다이렉트
         if(typeof window !== 'undefined') {
           window.location.href = PUBLIC_PAGE;
         }
