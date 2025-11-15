bff561f80a44d64754058b6d5ea5481ec76a93f5
Dino0204 <s24020@gsm.hs.kr>
2025-11-15 19:31:57 +0900

    feat: 사용되지 않는 GOOGLE_SIGNUP_URL 및 GOOGLE_SIGNIN_URL에 @deprecated 주석 추가

--- src/feature/google-auth/const/AUTH_URL.ts
+++ src/feature/google-auth/const/AUTH_URL.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated 선언되었으나 사용되지 않음.
+ */
 export const GOOGLE_SIGNUP_URL = `
 https://accounts.google.com/o/oauth2/v2/auth?
 response_type=code
@@ -6,6 +9,9 @@ response_type=code
 &redirect_uri=${process.env.NEXT_PUBLIC_GOOGLE_SIGNUP_REDIRECT_URI}
 `;
 
+/**
+ * @deprecated 선언되었으나 사용되지 않음.
+ */
 export const GOOGLE_SIGNIN_URL = `
 https://accounts.google.com/o/oauth2/v2/auth?
 response_type=code
--- src/shared/config/protect-page.ts
+++ src/shared/config/protect-page.ts
@@ -1,3 +1,6 @@
+/**
+ * @deprecated 선언되었으나 사용되지 않음.
+ */
 export const PROTECT_PAGE = [];
 
 export const PUBLIC_PAGE = '/';
