6e734812f75215c13d60ff4cd5e8d18e85f28412
bae080311 <s24024@gsm.hs.kr>
2025-11-17 01:02:09 +0900

    fix: feedback

--- src/shared/model/useAttachFile.ts
+++ src/shared/model/useAttachFile.ts
@@ -2,10 +2,10 @@ import { useMutation } from '@tanstack/react-query';
 import { attachFile } from '../api/attachFile';
 import { toast } from 'sonner';
 
-export const useAttachFile = (file: File) => {
+export const useAttachFile = () => {
   return useMutation({
-    mutationFn: () => attachFile(file),
+    mutationFn: attachFile,
     onSuccess: () => toast.success('파일이 업로드 되었습니다'),
-    onError: (e) => toast.error(e.message ?? '파일 업로드에 실패하였습니다'),
+    onError: (e: Error) => toast.error(e.message ?? '파일 업로드에 실패하였습니다'),
   });
 };
