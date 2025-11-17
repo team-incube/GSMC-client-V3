92a54b7b8f52056386fe5fced4899c6dd03954c2
bae080311 <s24024@gsm.hs.kr>
2025-11-17 17:17:21 +0900

    feat: return loading

--- src/shared/model/useAttachFile.ts
+++ src/shared/model/useAttachFile.ts
@@ -3,9 +3,14 @@ import { attachFile } from '../api/attachFile';
 import { toast } from 'sonner';
 
 export const useAttachFile = () => {
-  return useMutation({
+  const mutation = useMutation({
     mutationFn: attachFile,
     onSuccess: () => toast.success('파일이 업로드 되었습니다'),
     onError: (e: Error) => toast.error(e.message ?? '파일 업로드에 실패하였습니다'),
   });
+
+  return {
+    attachFile: mutation.mutate,
+    loading: mutation.isPending,
+  };
 };
