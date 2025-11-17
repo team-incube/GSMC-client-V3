c8296c7c20bee545513d37da4ad9eaabb2ca143a
Dino0204 <s24020@gsm.hs.kr>
2025-11-17 15:30:01 +0900

    fix: 숫자 타입 학번을 사용하도록 변경

--- src/feature/google-auth/lib/handleSignup.ts
+++ src/feature/google-auth/lib/handleSignup.ts
@@ -13,19 +13,22 @@ export async function handleSignup(
   formData: FormData,
 ): Promise<SignupFormState> {
   const name = String(formData.get('name') ?? '').trim();
-  const studentNumber = String(formData.get('studentNumber') ?? '').trim();
-
+  const studentNumber = Number(formData.get('studentNumber'));
   const result = SignupSchema.safeParse({ name, studentNumber });
+
   if (result.success) {
     const response = await postSignup({ name, studentNumber });
-    if (response.status === 202) {
-      toast.success('회원가입이 완료되었습니다. 다시 로그인해주세요.');
-      
-      axios.post('/api/auth/refresh');
-      
+    console.log('Signup API Response Status:', response.status);
+    if (response.code === 202) {
+      // toast.success('회원가입이 완료되었습니다. 다시 로그인해주세요.');
+
+      await axios.post('/api/auth/refresh');
+
       redirect('/main');
     }
-    toast.error(response.message ?? '회원가입에 실패했습니다.');
+
+    // toast.error(response.message ?? '회원가입에 실패했습니다.');
+
     return {
       ...prevState,
       name,
@@ -36,6 +39,7 @@ export async function handleSignup(
   }
 
   const tree = z.treeifyError(result.error);
+
   return {
     name,
     studentNumber,
