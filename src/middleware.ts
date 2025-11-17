a70a0337dabd35caf29230607fd6b597aa99bcbf
Dino0204 <s24020@gsm.hs.kr>
2025-11-17 16:07:23 +0900

    fix: Middleware Role 검증 로직

--- src/middleware.ts
+++ src/middleware.ts
@@ -34,16 +34,22 @@ export async function middleware(request: NextRequest) {
     console.log('Middleware User Role:', userRole);
   }
 
-  if (!hasAccessToken && isProtectedRoute) {
-    return NextResponse.redirect(new URL('/', request.url));
+  if (isProtectedRoute) {
+    if (!userRole) {
+      // 토큰이 없거나 유효하지 않으면 로그인 페이지로 리디렉션
+      return NextResponse.redirect(new URL('/', request.url));
+    }
+    if (userRole === 'UNAUTHORIZED') {
+      // 회원가입이 완료되지 않았으면 회원가입 페이지로 리디렉션
+      return NextResponse.redirect(new URL('/signup', request.url));
+    }
   }
 
-  if (hasAccessToken && userRole === 'UNAUTHORIZED' && isProtectedRoute) {
-    return NextResponse.redirect(new URL('/signup', request.url));
-  }
-
-  if (hasAccessToken && userRole !== 'UNAUTHORIZED' && isPublicRoute) {
-    return NextResponse.redirect(new URL('/main', request.url));
+  if (isPublicRoute) {
+    if (userRole && userRole !== 'UNAUTHORIZED') {
+      // 로그인 및 회원가입이 완료된 사용자가 공개 페이지 접근 시 메인 페이지로 리디렉션
+      return NextResponse.redirect(new URL('/main', request.url));
+    }
   }
 
   return NextResponse.next({
