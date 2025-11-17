dc6db9866e56722a2246d9ac9f6bc456b71a1d6b
Dino0204 <s24020@gsm.hs.kr>
2025-11-17 15:29:37 +0900

    feat: 요청 인터셉터에 액세스 토큰 추가 및 응답 인터셉터 제거

--- src/shared/lib/axios.ts
+++ src/shared/lib/axios.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosError, AxiosResponse, InternalAxiosRequestConfig } from 'axios';
 import { PUBLIC_PAGE } from '../config/protect-page';
+import { cookies } from 'next/headers';
 
 export const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
@@ -12,33 +13,18 @@ export const instance = axios.create({
   withCredentials: true,
 });
 
-instance.interceptors.response.use(
-  (response: AxiosResponse) => {
-    return response;
-  },
-  async (error: AxiosError) => {
-    
-    const originalRequest = error.config as InternalAxiosRequestConfig & { _retry?: boolean };
+instance.interceptors.request.use(
+  async (config: InternalAxiosRequestConfig) => {
+    const cookieStore = await cookies();
+    const accessToken = cookieStore.get('accessToken')?.value;
 
-    if (error.response?.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-
-      try {
-        // Route Handler API를 호출하여 토큰 갱신 시도
-        await axios.post('http://localhost:3000/api/auth/refresh',{},{withCredentials: true});
-        
-        // 토큰 갱신이 성공하면 원래 요청을 다시 시도
-        return instance(originalRequest);
-
-      } catch (error) {
-        // 토큰 갱신 실패 시 로그인 페이지로 리다이렉트
-        if(typeof window !== 'undefined') {
-          window.location.href = PUBLIC_PAGE;
-        }
-        return Promise.reject(error);
-      }
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
 
+    return config;
+  },
+  (error: AxiosError) => {
     return Promise.reject(error);
   },
 );
