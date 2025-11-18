ceecbdafd5649a8f91528943f9980252f6ff6b20
Dino0204 <s24020@gsm.hs.kr>
2025-11-18 15:33:08 +0900

    refactor: dynamic import 를 사용하는 instance로 변경

--- src/shared/lib/instance.ts
+++ src/shared/lib/instance.ts
@@ -1,12 +1,12 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
-import { cookies } from 'next/headers';
+import { getCookie } from './cookie/cookie';
 
 export const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
 /**
- * 서버 사이드 전용 Axios 인스턴스
+ * 클라이언트/서버 사이드 동적 Axios 인스턴스
  */
-export const serverInstance = axios.create({
+export const instance = axios.create({
   baseURL,
   timeout: 5000,
   headers: {
@@ -15,10 +15,17 @@ export const serverInstance = axios.create({
   withCredentials: true,
 });
 
-serverInstance.interceptors.request.use(
+instance.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
-    const cookieStore = await cookies();
-    const accessToken = cookieStore.get('accessToken')?.value;
+    let accessToken;
+
+    if (typeof window === 'undefined') {
+      const { cookies } = await import('next/headers');
+      const cookieStore = await cookies();
+      accessToken = cookieStore.get('accessToken')?.value || null;
+    } else {
+      accessToken = getCookie('accessToken');
+    }
 
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
