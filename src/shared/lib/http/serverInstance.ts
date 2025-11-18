ee9da65aee377176952f9fdaecb7c554a48d0881
Dino0204 <s24020@gsm.hs.kr>
2025-11-18 15:03:13 +0900

    refactor: instance가 각 환경에 맞게 실행되도록 분리

--- src/shared/lib/http/serverInstance.ts
+++ src/shared/lib/http/serverInstance.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosError, InternalAxiosRequestConfig } from 'axios';
 import { cookies } from 'next/headers';
-import { getCookie } from './cookie';
 
 export const baseURL = process.env.NEXT_PUBLIC_API_URL;
 
-export const instance = axios.create({
+/**
+ * 서버 사이드 전용 Axios 인스턴스
+ */
+export const serverInstance = axios.create({
   baseURL,
   timeout: 5000,
   headers: {
@@ -13,10 +15,10 @@ export const instance = axios.create({
   withCredentials: true,
 });
 
-instance.interceptors.request.use(
+serverInstance.interceptors.request.use(
   async (config: InternalAxiosRequestConfig) => {
     const cookieStore = await cookies();
-    const accessToken = cookieStore.get('accessToken')?.value || getCookie('accessToken');
+    const accessToken = cookieStore.get('accessToken')?.value;
 
     if (accessToken) {
       config.headers.Authorization = `Bearer ${accessToken}`;
