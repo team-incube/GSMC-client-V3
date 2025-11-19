6716f85701fb51856cd1cdcef68d3a6de0d76d22
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 04:56:36 +0900

    feat: getScores

--- src/entities/score/api/getScores.ts
+++ src/entities/score/api/getScores.ts
@@ -1,1 +1,13 @@
-// 보류
+import { instance } from '@/shared/lib/instance';
+import { CategoryKey } from '@/shared/type/category';
+import { ScoreStatus, ScoreType } from '../model/score';
+
+export interface GetScoresParams {
+  categoryType?: CategoryKey;
+  status?: ScoreStatus;
+}
+
+export const getScores = async (params: GetScoresParams): Promise<ScoreType[]> => {
+  const response = await instance.get(`/scores`, { params });
+  return response.data;
+};
