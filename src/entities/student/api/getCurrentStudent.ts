816722189db1ebb20ed6a5e60ee202b2ed251897
Dino0204 <s24020@gsm.hs.kr>
2025-11-19 15:03:48 +0900

    remove: 주석 제거

--- src/entities/student/api/getCurrentStudent.ts
+++ src/entities/student/api/getCurrentStudent.ts
@@ -1,11 +1,6 @@
 import { instance } from '@/shared/lib/instance';
 import type { StudentType } from '../model/StudentSchema';
 
-/**
- * 현재 인증된 사용자 정보 조회
- * @Todo MainView에서 사용 예정
- * @status UNUSED - 구현됨, 아직 사용되지 않음
- */
 export const getCurrentStudent = async (): Promise<StudentType> => {
   const response = await instance.get('/members/current');
   return response.data.data;
