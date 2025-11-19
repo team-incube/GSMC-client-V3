fb7d5fe7b33c0597f4c85162a432b8e1c42c001a
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 04:55:19 +0900

    fix: 단수형 -> 복수형

--- src/entities/evidence/model/EvidenceSchema.ts
+++ src/entities/evidence/model/EvidenceSchema.ts
@@ -4,5 +4,5 @@ export const EvidenceSchema = z.object({
   scoreId: z.number().min(1, 'scoreId가 누락되었습니다'),
   title: z.string().min(1, '제목을 입력해주세요.'),
   content: z.string().min(1, '내용을 입력해주세요.'),
-  fileId: z.array(z.number()).nonempty('파일첨부는 필수입니다'),
+  fileIds: z.array(z.number()).nonempty('파일첨부는 필수입니다'),
 });
