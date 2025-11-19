ea87f6f9e6a76206ee8b9763ad96cf819ee60f1e
Dino0204 <s24020@gsm.hs.kr>
2025-11-19 15:03:31 +0900

    remove: 사용되지 않는 학생 반환 타입 제거

--- src/entities/student/model/StudentSchema.ts
+++ src/entities/student/model/StudentSchema.ts
@@ -3,19 +3,14 @@ import z from 'zod';
 export const RoleEnum = z.enum(['UNAUTHORIZED', 'STUDENT', 'TEACHER', 'ROOT']);
 
 export const StudentSchema = z.object({
-  id: z.string(),
+  id: z.number(),
   name: z.string(),
   email: z.string(),
   grade: z.number(),
-  class: z.number(),
+  classNumber: z.number(),
   number: z.number(),
   role: RoleEnum,
 });
 
-export const StudentResponse = z.object({
-  data: StudentSchema,
-});
-
 export type StudentType = z.infer<typeof StudentSchema>;
-export type StudentResponseType = z.infer<typeof StudentResponse>;
 export type RoleType = z.infer<typeof RoleEnum>;
