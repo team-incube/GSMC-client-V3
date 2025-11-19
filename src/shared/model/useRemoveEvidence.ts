f100fab6c67d1b7e149d7bce94fbd1ca9f95793a
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 03:14:53 +0900

    feat: useRemoveEvidence

---
+++ src/shared/model/useRemoveEvidence.ts
@@ -0,0 +1,9 @@
+import { useMutation } from '@tanstack/react-query';
+import { removeEvidence } from '../api/removeEvidence';
+
+export const useRemoveEvidence = () => {
+  return useMutation({
+    mutationKey: ['evidence'],
+    mutationFn: removeEvidence,
+  });
+};
