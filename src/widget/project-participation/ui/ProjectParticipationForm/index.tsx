50bafefb6541da3936c5a6c94dcd93626c96d802
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 04:58:24 +0900

    refactor: projectParticipationForm

--- src/widget/project-participation/ui/ProjectParticipationForm/index.tsx
+++ src/widget/project-participation/ui/ProjectParticipationForm/index.tsx
@@ -1,6 +1,8 @@
 'use client';
 
-import { handleProjectSubmit, initialState, SubmitState } from '@/feature/project-participation/lib/handleProjectSubmit';
+import { handleProjectParticipation } from '@/feature/project-participation/lib/handleProjectParticipation';
+import { ParticipationProjectFormState } from '@/feature/project-participation/model/ParticipationProjectForm';
+import { createInitialState } from '@/shared/lib/createInitialState';
 import Button from '@/shared/ui/Button';
 import FileUploader from '@/shared/ui/FileUploader';
 import Input from '@/shared/ui/Input';
@@ -10,37 +12,34 @@ import { useEffect } from 'react';
 import { toast } from 'sonner';
 
 export default function ProjectParticipationForm() {
-  const [state, formAction, isPending] = useActionState<SubmitState, FormData>(
-    handleProjectSubmit,
-    initialState,
-  );
+  const [state, formAction, isPending] = useActionState(handleProjectParticipation, createInitialState<ParticipationProjectFormState>(),);
 
   useEffect(() => {
     if (state.message) {
-      if (state.success) {
+      if (state.status == "success") {
         toast.success(state.message);
       } else {
         toast.error(state.message);
       }
     }
-  }, [state.message, state.success]);
+  }, [state]);
 
   return (
     <form className="flex flex-col w-full gap-16" action={formAction}>
       <div className="flex flex-col gap-6">
         <Input name="title" placeholder="주제를 입력해주세요" label="주제" />
-        {!!state.titleError && <p className="text-xs text-red-500">{state.titleError}</p>}
+        <small className="text-error pl-1">{state.fieldErrors?.title}</small>
         <Textarea name="content" placeholder="내용을 입력해주세요" label="내용" />
-        {!!state.contentError && <p className="text-xs text-red-500">{state.contentError}</p>}
-        <FileUploader name="image" label="이미지" placeholder="파일첨부" />
-        {!!state.fileError && <p className="text-xs text-red-500">{state.fileError}</p>}
+        <small className="text-error pl-1">{state.fieldErrors?.content}</small>
+        <FileUploader name="fileIds" label="파일" placeholder="파일을 업로드해주세요" />
+        <small className="text-error pl-1">{state.fieldErrors?.fileIds}</small>
       </div>
 
       <div className="flex flex-col gap-[10px]">
         <Button type="button" variant="border">
           임시저장
         </Button>
-        <Button type="submit" disabled={Boolean(isPending && state.success)}>
+        <Button type="submit">
           {isPending ? '작성 중...' : '작성 완료'}
         </Button>
       </div>
