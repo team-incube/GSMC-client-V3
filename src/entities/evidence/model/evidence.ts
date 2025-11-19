e0d1720f9c5ca614cedfb63b80f4bf24599d1965
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 04:56:13 +0900

    fix: type 명 수정

--- src/entities/evidence/model/evidence.ts
+++ src/entities/evidence/model/evidence.ts
@@ -1,18 +1,10 @@
 import { FileType } from '@/entities/file/model/file';
 
-export interface Evidence {
-  scoreId: number;
+export interface EvidenceType {
+  evidenceId: number;
   title: string;
   content: string;
-}
-
-export interface EvidenceResponseType extends Evidence {
-  id: string;
-  createAt: string;
-  updateAt: string;
-  file: FileType[];
-}
-
-export interface EvidenceRequestType extends Evidence {
-  file: number[];
+  createdAt: string;
+  updatedAt: string;
+  fileIds: FileType[] | null;
 }
