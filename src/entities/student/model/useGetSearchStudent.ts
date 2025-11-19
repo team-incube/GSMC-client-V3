4a5b79f8eac674efd74e256461c26d296d21ffba
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 03:39:38 +0900

    remove: 추가하지 않아도 되는 타입

--- src/entities/student/model/useGetSearchStudent.ts
+++ src/entities/student/model/useGetSearchStudent.ts
@@ -1,12 +1,11 @@
 import { useQuery } from '@tanstack/react-query';
 import { getSearchStudent, GetSearchStudentRequest } from '../api/getSearchStudent';
-import { StudentType } from '../model/StudentSchema';
 import { useDebouncedValue } from '@/shared/model/useDebouncedValue';
 
 export const useGetSearchStudent = (params: GetSearchStudentRequest) => {
   const debouncedParams = useDebouncedValue(params, 500);
 
-  return useQuery<StudentType[]>({
+  return useQuery({
     queryKey: ['searchStudent', debouncedParams],
     queryFn: () => getSearchStudent(debouncedParams),
     enabled: !!debouncedParams.name?.trim(),
