85d9d8a7a66f4b85e87a155914b3f23f2128ed80
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 11:29:56 +0900

    feat: project 정보를 받아오도록 변경

--- src/widget/project-participation/ui/ProjectParticipationForm/index.tsx
+++ src/widget/project-participation/ui/ProjectParticipationForm/index.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { useGetProjectById } from '@/entities/project/model/useGetProjectById';
 import { handleProjectParticipation } from '@/feature/project-participation/lib/handleProjectParticipation';
 import { ParticipationProjectFormState } from '@/feature/project-participation/model/ParticipationProjectForm';
 import { createInitialState } from '@/shared/lib/createInitialState';
@@ -7,12 +8,15 @@ import Button from '@/shared/ui/Button';
 import FileUploader from '@/shared/ui/FileUploader';
 import Input from '@/shared/ui/Input';
 import Textarea from '@/shared/ui/Textarea';
+import { useParams } from 'next/navigation';
 import { useActionState } from 'react';
 import { useEffect } from 'react';
 import { toast } from 'sonner';
 
 export default function ProjectParticipationForm() {
   const [state, formAction, isPending] = useActionState(handleProjectParticipation, createInitialState<ParticipationProjectFormState>(),);
+  const { id: projectId } = useParams();
+  const { data: project } = useGetProjectById(Number(projectId));
 
   useEffect(() => {
     if (state.message) {
@@ -27,19 +31,20 @@ export default function ProjectParticipationForm() {
   return (
     <form className="flex flex-col w-full gap-16" action={formAction}>
       <div className="flex flex-col gap-6">
-        <Input name="title" placeholder="주제를 입력해주세요" label="주제" />
+        <Input name="title" placeholder="제목을 입력해주세요" label="제목" defaultValue={project?.title} readOnly />
         <small className="text-error pl-1">{state.fieldErrors?.title}</small>
-        <Textarea name="content" placeholder="내용을 입력해주세요" label="내용" />
+        <Textarea name="content" placeholder="프로젝트에서 했던 활동을 입력해주세요" label="내용" />
         <small className="text-error pl-1">{state.fieldErrors?.content}</small>
         <FileUploader name="fileIds" label="파일" placeholder="파일을 업로드해주세요" />
         <small className="text-error pl-1">{state.fieldErrors?.fileIds}</small>
+        <Input name="projectId" type="hidden" value={projectId} readOnly />
       </div>
 
       <div className="flex flex-col gap-[10px]">
         <Button type="button" variant="border">
           임시저장
         </Button>
-        <Button type="submit">
+        <Button type="submit" disabled={isPending}>
           {isPending ? '작성 중...' : '작성 완료'}
         </Button>
       </div>
