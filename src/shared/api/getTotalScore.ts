0989278fd18b15a5d26c0ea551f0e25b9405b62e
Dino0204 <s24020@gsm.hs.kr>
2025-11-20 23:00:49 +0900

    fix: data 반환 타입

--- src/shared/api/getTotalScore.ts
+++ src/shared/api/getTotalScore.ts
@@ -1,10 +1,6 @@
 import { instance } from '../lib/instance';
 
-/**
- * 사용자 점수 총점 반환
- */
 export const getTotalScore = async (): Promise<{ totalScore: string }> => {
   const response = await instance.get('/scores/total');
-  console.log(response);
-  return response.data;
+  return response.data.data;
 };
