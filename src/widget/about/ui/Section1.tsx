cdc7ed8aab752e34981d368d58c80f91f5a987e2
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-22 09:06:31 +0900

    feat: 인증제 소개 화면 첫부분 컴포넌트화

---
+++ src/widget/about/ui/Section1.tsx
@@ -0,0 +1,21 @@
+import Header from "@/shared/ui/header/ui/Header";
+import ScrollDown from "@/shared/asset/svg/ScrollDown";
+
+
+export default function Section1() {
+    return (
+        <div className="w-full">
+            <Header />
+            <div className="relative bg-[url(@/shared/asset/img/aboutImg.png)] h-[calc(100vh-4.375rem)] bg-cover bg-center">
+                <div className="absolute inset-0 bg-black/60"></div>
+                    <div className="absolute flex flex-col items-center justify-between inset-0 ">
+                        <div></div>
+                        <p className="font-extrabold text-[3.5rem] text-white">GSM 인증제란?</p>
+                        <ScrollDown onClick={() => {
+                            document.getElementById('section2')?.scrollIntoView({ behavior: 'smooth' });
+                        }}/>
+                    </div>
+            </div>
+        </div>
+    )
+}
