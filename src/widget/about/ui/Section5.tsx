5a7b78163aa5ea3798854f4e9a80117e65eeec6d
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-22 21:18:37 +0900

    refactor: 고정 너비 제거 후 w-full + max-w 적용

--- src/widget/about/ui/Section5.tsx
+++ src/widget/about/ui/Section5.tsx
@@ -4,7 +4,7 @@ export default function Section5() {
     return (
         <section className="flex flex-col items-center gap-[6rem]">
             <p className="text-[2.75rem] font-bold text-center">운영 방식</p>
-            <div className="flex justify-center flex-wrap flex-row gap-[4.25rem] w-[100rem]">
+            <div className="flex justify-center flex-wrap flex-row gap-[4.25rem] w-full max-w-[100rem]">
                 {operateorderData.map((row) => (
                     <div key={row.id} className="flex flex-row items-center w-[47.3125rem] h-[9.75rem] rounded-[5rem] bg-[#131E32] px-[2.25rem] py-[1.75rem] gap-[1.25rem]">
                         <div className="flex items-center justify-center w-[6.25rem] h-[6.25rem] font-semibold text-[2.5rem] text-[#385B97] bg-[#DFEAFE] rounded-[11.25rem]">
