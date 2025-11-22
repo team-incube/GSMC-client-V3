4c54d9277d7950fd2db47e04f66e9e6923ddb6a3
Dino0204 <s24020@gsm.hs.kr>
2025-11-22 15:23:15 +0900

    refactor: ScorePatchModal 컴포넌트로 분리

--- src/view/main/ui/MainView/index.tsx
+++ src/view/main/ui/MainView/index.tsx
@@ -7,11 +7,10 @@ import { useGetProjects } from "@/entities/project/model/useGetProjects";
 import { useGetcoresByCategory } from "@/entities/score/model/useGetScoresByCategory";
 import { useGetTotalScore } from "@/entities/score/model/useGetTotalScore";
 import { useGetCurrentStudent } from "@/entities/student/model/useGetCurrentStudent";
-import { cn } from "@/shared/lib/cn";
 import Button from "@/shared/ui/Button";
-import ModalWrapper from "@/shared/ui/ModalWrapper";
 import ProjectPost from "@/shared/ui/ProjectPost";
 import SearchBar from "@/shared/ui/SearchBar";
+import ScorePatchModal from "@/widget/main/ui/ScorePatchModal";
 
 export default function MainView() {
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -26,49 +25,7 @@ export default function MainView() {
 
   return (
     <div className="flex flex-col w-full">
-      {isModalOpen ? <ModalWrapper>
-        <div className="flex flex-col justify-center gap-10 w-150 overflow-hidden">
-          <h2 className="text-titleMedium text-center">
-            내 점수 수정하기
-          </h2>
-          <section className="flex flex-col justify-start items-start max-h-160 overflow-y-scroll">
-            {scoresByCategory?.map((category) => (
-              <div key={category.categoryType} className="w-full flex flex-col">
-                <div className="flex justify-between px-5 py-2 bg-gray-50 text-sm font-bold text-gray-500">
-                  <p>{category.categoryNames.koreanName}</p>
-                  <p>{category.recognizedScore}점</p>
-                </div>
-                {category.scores.map((score) => (
-                  <article
-                    key={score.scoreId}
-                    className="flex justify-between items-center w-full px-5 py-4 border-b border-gray-100 last:border-none"
-                  >
-                    <div className="flex items-center gap-[0.75rem]">
-                      <div className={cn("w-2 h-2 aspect-square rounded-full", {
-                        "bg-main-500": score.scoreStatus === "APPROVED",
-                        "bg-gray-600": score.scoreStatus === "REJECTED",
-                        "bg-error": score.scoreStatus === "PENDING",
-                      })} />
-                      <p className="text-body2">
-                        {score.activityName}
-                      </p>
-                    </div>
-                    <div className="flex items-center gap-[0.75rem]">
-                      <Button variant="border" className="w-auto px-[0.75rem] py-[0.125rem]">
-                        수정
-                      </Button>
-                    </div>
-                  </article>
-                ))}
-              </div>
-            ))}
-          </section>
-        </div>
-        <Button variant="border" onClick={() => setIsModalOpen(false)}>
-          뒤로가기
-        </Button>
-      </ModalWrapper> : null
-      }
+      <ScorePatchModal isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} scoresByCategory={scoresByCategory} />
       <section className="flex justify-start w-full">
         <div className="flex flex-col font-semibold w-72 h-[145px] gap-[27px]">
           <div className="flex items-baseline gap-3">
---
+++ src/widget/main/ui/ScorePatchModal/index.tsx
@@ -0,0 +1,55 @@
+import { getScoresByCategoryResponse } from "@/entities/score/api/getScoresByCategory";
+import { cn } from "@/shared/lib/cn";
+import Button from "@/shared/ui/Button";
+import ModalWrapper from "@/shared/ui/ModalWrapper";
+
+export default function ScorePatchModal({ isModalOpen = false, setIsModalOpen, scoresByCategory }: { isModalOpen: boolean; setIsModalOpen: (isOpen: boolean) => void; scoresByCategory: getScoresByCategoryResponse[] | undefined }) {
+  return (
+    <div>
+      {isModalOpen ? <ModalWrapper>
+        <div className="flex flex-col justify-center gap-10 w-150 overflow-hidden">
+          <h2 className="text-titleMedium text-center">
+            내 점수 수정하기
+          </h2>
+          <section className="flex flex-col justify-start items-start max-h-160 overflow-y-scroll">
+            {scoresByCategory?.map((category) => (
+              <div key={category.categoryType} className="w-full flex flex-col">
+                <div className="flex justify-between px-5 py-2 bg-gray-50 text-sm font-bold text-gray-500">
+                  <p>{category.categoryNames.koreanName}</p>
+                  <p>{category.recognizedScore}점</p>
+                </div>
+                {category.scores.map((score) => (
+                  <article
+                    key={score.scoreId}
+                    className="flex justify-between items-center w-full px-5 py-4 border-b border-gray-100 last:border-none"
+                  >
+                    <div className="flex items-center gap-[0.75rem]">
+                      <div className={cn("w-2 h-2 aspect-square rounded-full", {
+                        "bg-main-500": score.scoreStatus === "APPROVED",
+                        "bg-gray-600": score.scoreStatus === "REJECTED",
+                        "bg-error": score.scoreStatus === "PENDING",
+                      })} />
+                      <p className="text-body2">
+                        {score.activityName}
+                      </p>
+                    </div>
+                    <div className="flex items-center gap-[0.75rem]">
+                      <Button variant="border" className="w-auto px-[0.75rem] py-[0.125rem]">
+                        수정
+                      </Button>
+                    </div>
+                  </article>
+                ))}
+              </div>
+            ))}
+          </section>
+        </div>
+        <Button variant="border" onClick={() => setIsModalOpen(false)}>
+          뒤로가기
+        </Button>
+      </ModalWrapper> : null
+      }
+    </div>
+
+  )
+}
