f24612e22f40523a6704cd167a290f3243d3a5b2
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-22 21:54:52 +0900

    refactor: 이미지 alt 내용 종이비행기 아이콘으로 변경

--- src/widget/about/ui/Section2.tsx
+++ src/widget/about/ui/Section2.tsx
@@ -7,7 +7,7 @@ export default function Section2() {
             <p className="text-[2.75rem] font-bold text-center">프로그램 소개</p>
             <div className="flex flex-col w-[70rem] h-[32.5625rem] border border-[#CDCDCF] rounded-[1.25rem] px-[3.75rem] py-[3.75rem] gap-[2.5rem]">
                 <div className="flex flex-row gap-[1.25rem]">
-                    <Image alt="gsmc" src={airplane} width={50} height={50} className="mb-[3rem]"/>
+                    <Image alt="종이비행기 아이콘" src={airplane} width={50} height={50} className="mb-[3rem]"/>
                     <div>
                         <p className="font-semibold text-[2rem]">역량인증 기준 설정</p>
                         <p className="font-light text-xl">GSM 인증제는 영마이스터로서 졸업할 때까지 <span className="font-semibold text-[#385B97]">적정 수준에 도달</span>할 수 있도록<br/>학교 실정에 맞게 역량인증의 기준을 설정하여 운영됩니다.</p>
@@ -15,7 +15,7 @@ export default function Section2() {
                 </div>
                 <hr className="w-[1000px] border-[#CDCDCF]"/>
                 <div className="flex flex-row gap-[1.25rem]">
-                    <Image alt="logo" src={airplane} width={50} height={50} className="mb-[3rem]"/>
+                    <Image alt="종이비행기 아이콘" src={airplane} width={50} height={50} className="mb-[3rem]"/>
                     <div>
                         <p className="font-semibold text-[2rem]">동기 부여 및 책무성 제고</p>
                         <p className="font-light text-xl">일정 수준에 도달한 학생에게 인증서를 수여함으로써 학생들의 <span className="font-semibold text-[#385B97]">교육 참여 동기를 부여</span>하고<br/>학교 교육의 <span className="font-semibold text-[#385B97]">책무성을 제고</span>하고자 하는 프로그램입니다.</p>
