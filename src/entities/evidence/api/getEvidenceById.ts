84959369bf0e060a9ca1ced47678e4bc60e7cbe2
Dino0204 <s24020@gsm.hs.kr>
2025-11-23 16:35:29 +0900

    fix: evidence api

--- src/entities/evidence/api/getEvidenceById.ts
+++ src/entities/evidence/api/getEvidenceById.ts
@@ -1,10 +1,13 @@
+import { EvidenceType } from '@/shared/api/getCategories';
 import { instance } from '@/shared/lib/instance';
 
 export interface getScoreByIdRequest {
   evidenceId: string;
 }
 
-export const getEvidenceById = async ({ evidenceId }: getScoreByIdRequest) => {
+export const getEvidenceById = async ({
+  evidenceId,
+}: getScoreByIdRequest): Promise<EvidenceType> => {
   const response = await instance.get(`/evidences/${evidenceId}`);
   return response.data.data;
 };
