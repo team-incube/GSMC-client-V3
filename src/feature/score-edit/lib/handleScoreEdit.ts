9c4591d4031481559b2086c5bb73ee6b06742b96
Dino0204 <s24020@gsm.hs.kr>
2025-11-23 23:08:39 +0900

    feat: handleEvidenceEdit

---
+++ src/feature/score-edit/lib/handleScoreEdit.ts
@@ -0,0 +1,45 @@
+'use server';
+
+import z from 'zod';
+
+import { addScoreByategoryType } from '@/shared/api/addScoreByCategoryType';
+import { ActionState } from '@/shared/type/actionState';
+
+import { ScoreFormSchema, ScoreFormValueType } from '../model/ScoreEditSchema';
+
+export async function handleScoreValueEdit(
+  _prevState: ActionState<ScoreFormValueType>,
+  formData: FormData,
+): Promise<ActionState<ScoreFormValueType>> {
+  try {
+    const fileIdRaw = formData.get('fileId');
+    const fileId = fileIdRaw ? Number(fileIdRaw) : null;
+
+    const currentData: ScoreFormValueType = {
+      categoryType: String(formData.get('categoryType') ?? ''),
+      value: String(formData.get('value') ?? '').trim(),
+      fileId: fileId,
+    };
+
+    const result = ScoreFormSchema.safeParse(currentData);
+
+    if (!result.success) {
+      return {
+        status: 'error',
+        message: '입력값을 확인해주세요.',
+        fieldErrors: z.flattenError(result.error).fieldErrors,
+        data: currentData,
+      };
+    }
+
+    const response = await addScoreByategoryType({
+      categoryType: result.data.categoryType,
+      value: result.data.value,
+      fileId: result.data.fileId || undefined,
+    });
+
+    return { status: 'success', message: '수정되었습니다.', fieldErrors: null, data: null };
+  } catch (error) {
+    return { status: 'error', message: '수정에 실패했습니다.', fieldErrors: null, data: null };
+  }
+}
