85732a2f97df6efdb1734e57d6b3fe0f318c33fc
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-24 23:53:41 +0900

    fix: 지워버린 padding, max-width 추가

--- src/view/main/ui/MainView/index.tsx
+++ src/view/main/ui/MainView/index.tsx
@@ -27,68 +27,70 @@ export default function MainView() {
   const projects = searchKeyword ? searchedProjects : allProjects;
 
   return (
-    <div className="flex w-full flex-col">
-      {isModalOpen ? <ScoreManagementModal setIsModalOpen={setIsModalOpen} /> : null}
-      <section className="flex w-full justify-start">
-        <div className="flex h-[145px] w-72 flex-col gap-[27px] font-semibold">
-          <div className="flex items-baseline gap-3">
-            <p className="text-main-700 text-center text-4xl">{student?.name}</p>
-            <p className="text-left text-2xl text-black">님의 인증제 점수는</p>
-          </div>
-          <div className="flex items-baseline gap-4.5">
-            <div className="flex items-center justify-center gap-2.5 rounded-full bg-[#f3f3f3] px-9 py-3">
-              <p className="text-main-500 flex-shrink-0 flex-grow-0 text-center text-5xl">
-                {score?.totalScore}점
-              </p>
+    <div className="flex w-full justify-center px-4 py-15.5">
+      <div className="flex w-full max-w-[600px] flex-col">
+        {isModalOpen ? <ScoreManagementModal setIsModalOpen={setIsModalOpen} /> : null}
+        <section className="flex w-full justify-start">
+          <div className="flex h-[145px] w-72 flex-col gap-[27px] font-semibold">
+            <div className="flex items-baseline gap-3">
+              <p className="text-main-700 text-center text-4xl">{student?.name}</p>
+              <p className="text-left text-2xl text-black">님의 인증제 점수는</p>
+            </div>
+            <div className="flex items-baseline gap-4.5">
+              <div className="flex items-center justify-center gap-2.5 rounded-full bg-[#f3f3f3] px-9 py-3">
+                <p className="text-main-500 flex-shrink-0 flex-grow-0 text-center text-5xl">
+                  {score?.totalScore}점
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="mt-[3.63rem]">
-        <div className="bg-main-100 w-full overflow-hidden rounded-2xl">
-          <div className="bg-main-100 flex items-center justify-between px-[2.25rem] py-[1.5rem]">
-            <h3 className="text-main-700 text-2xl font-semibold">내 점수</h3>
-            <Button
-              variant="active"
-              onClick={() => setIsModalOpen(true)}
-              className="w-auto px-[22px] py-[13px] text-center text-lg font-semibold"
-            >
-              점수 수정
-            </Button>
-          </div>
+        <section className="mt-[3.63rem]">
+          <div className="bg-main-100 w-full overflow-hidden rounded-2xl">
+            <div className="bg-main-100 flex items-center justify-between px-[2.25rem] py-[1.5rem]">
+              <h3 className="text-main-700 text-2xl font-semibold">내 점수</h3>
+              <Button
+                variant="active"
+                onClick={() => setIsModalOpen(true)}
+                className="w-auto px-[22px] py-[13px] text-center text-lg font-semibold"
+              >
+                점수 수정
+              </Button>
+            </div>
 
-          <div className="flex h-full rounded-xl px-[2.25rem]">
-            <div className="flex max-h-100 w-full flex-col items-start justify-start overflow-y-auto rounded-xl bg-white">
-              {scoresByCategory?.map((category) => (
-                <article
-                  key={category.categoryType}
-                  className="flex w-full items-center justify-between px-8 py-6"
-                >
-                  <p className="text-center text-lg font-semibold text-[#68696c]">
-                    {category.categoryNames.koreanName}
-                  </p>
-                  <p className="text-center text-lg font-semibold text-[#68696c]">
-                    {category.recognizedScore}점
-                  </p>
-                </article>
-              ))}
+            <div className="flex h-full rounded-xl px-[2.25rem]">
+              <div className="flex max-h-100 w-full flex-col items-start justify-start overflow-y-auto rounded-xl bg-white">
+                {scoresByCategory?.map((category) => (
+                  <article
+                    key={category.categoryType}
+                    className="flex w-full items-center justify-between px-8 py-6"
+                  >
+                    <p className="text-center text-lg font-semibold text-[#68696c]">
+                      {category.categoryNames.koreanName}
+                    </p>
+                    <p className="text-center text-lg font-semibold text-[#68696c]">
+                      {category.recognizedScore}점
+                    </p>
+                  </article>
+                ))}
+              </div>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="mt-6 flex flex-col gap-6">
-        <SearchBar
-          placeholder="찾는 내 프로젝트를 입력해주세요."
-          onSearchChange={setSearchKeyword}
-        />
-        <div className="flex flex-wrap gap-4">
-          {projects?.map((project) => (
-            <ProjectPost key={project.id} {...project} />
-          ))}
-        </div>
-      </section>
+        <section className="mt-6 flex flex-col gap-6">
+          <SearchBar
+            placeholder="찾는 내 프로젝트를 입력해주세요."
+            onSearchChange={setSearchKeyword}
+          />
+          <div className="flex flex-wrap gap-4">
+            {projects?.map((project) => (
+              <ProjectPost key={project.id} {...project} />
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
