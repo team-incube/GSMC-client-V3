9c9fc48d29c5f4a90d4be1d1e5ffefdf8c947b63
jyuuuuu0 <s25023@gsm.hs.kr>
2025-11-24 20:28:48 +0900

    feat: padding, max-width 추가

--- src/view/main/ui/MainView/index.tsx
+++ src/view/main/ui/MainView/index.tsx
@@ -1,15 +1,15 @@
-"use client"
+'use client';
 
-import { useState } from "react";
+import { useState } from 'react';
 
-import { useGetProjectBySearch } from "@/entities/project/model/useGetProjectBySearch";
-import { useGetProjects } from "@/entities/project/model/useGetProjects";
-import { useGetcoresByCategory } from "@/entities/score/model/useGetScoresByCategory";
-import { useGetTotalScore } from "@/entities/score/model/useGetTotalScore";
-import { useGetCurrentStudent } from "@/entities/student/model/useGetCurrentStudent";
-import Button from "@/shared/ui/Button";
-import ProjectPost from "@/shared/ui/ProjectPost";
-import SearchBar from "@/shared/ui/SearchBar";
+import { useGetProjectBySearch } from '@/entities/project/model/useGetProjectBySearch';
+import { useGetProjects } from '@/entities/project/model/useGetProjects';
+import { useGetcoresByCategory } from '@/entities/score/model/useGetScoresByCategory';
+import { useGetTotalScore } from '@/entities/score/model/useGetTotalScore';
+import { useGetCurrentStudent } from '@/entities/student/model/useGetCurrentStudent';
+import Button from '@/shared/ui/Button';
+import ProjectPost from '@/shared/ui/ProjectPost';
+import SearchBar from '@/shared/ui/SearchBar';
 
 export default function MainView() {
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -17,66 +17,77 @@ export default function MainView() {
   const { data: score } = useGetTotalScore();
   const { data: scoresByCategory } = useGetcoresByCategory({ status: 'APPROVED' });
   const { data: allProjects } = useGetProjects();
-  const { data: searchedProjects } = useGetProjectBySearch({ title: searchKeyword, page: 0, size: 10 });
+  const { data: searchedProjects } = useGetProjectBySearch({
+    title: searchKeyword,
+    page: 0,
+    size: 10,
+  });
   const projects = searchKeyword ? searchedProjects : allProjects;
 
   return (
-    <div className="flex flex-col w-full">
-
-      <section className="flex justify-start w-full">
-        <div className="flex flex-col font-semibold w-72 h-[145px] gap-[27px]">
-          <div className="flex items-baseline gap-3">
-            <p className="text-4xl text-center text-main-700">{student?.name}</p>
-            <p className="text-2xl text-left text-black">님의 인증제 점수는</p>
-          </div>
-          <div className="flex items-baseline gap-4.5">
-            <div className="flex justify-center items-center gap-2.5 px-9 py-3 rounded-full bg-[#f3f3f3]">
-              <p className="flex-grow-0 flex-shrink-0 text-5xl text-center text-main-500">{score?.totalScore}점</p>
+    <div className="flex w-full justify-center px-4 py-15.5">
+      <div className="flex w-full max-w-[600px] flex-col">
+        <section className="flex w-full justify-start">
+          <div className="flex h-[145px] w-72 flex-col gap-[27px] font-semibold">
+            <div className="flex items-baseline gap-3">
+              <p className="text-main-700 text-center text-4xl">{student?.name}</p>
+              <p className="text-left text-2xl text-black">님의 인증제 점수는</p>
             </div>
-            <p className="text-2xl text-black">입니다.</p>
-          </div>
-        </div>
-      </section>
-
-      <section className="mt-[3.63rem]">
-        <div className="w-full h-[445px] relative overflow-hidden rounded-[20px] bg-[#dfeafe]">
-
-          <div className="flex justify-between items-center py-[1.5rem] bg-[#dfeafe] px-[2.25rem]">
-            <p className="text-2xl font-semibold text-[#385b97]">내 점수</p>
-            <Button variant="active" className="w-auto px-[22px] py-[13px] text-lg font-semibold text-center">
-              점수 수정
-            </Button>
-          </div>
-
-          <div className="flex flex-col justify-start items-start h-full overflow-y-scroll rounded-xl px-[2.25rem]">
-            <div className="flex flex-col justify-start items-start flex-grow-0 flex-shrink-0 w-full bg-white rounded-xl">
-              {scoresByCategory?.map((category) => (
-                <article key={category.categoryType} className="flex justify-between items-center self-stretch flex-grow-0 flex-shrink-0 px-8 py-6">
-                  <p className="text-lg font-semibold text-center text-[#68696c]">{category.categoryNames.koreanName}</p>
-                  <p className="text-lg font-semibold text-center text-[#68696c]">{category.recognizedScore}점</p>
-                </article>
-              ))}
+            <div className="flex items-baseline gap-4.5">
+              <div className="flex items-center justify-center gap-2.5 rounded-full bg-[#f3f3f3] px-9 py-3">
+                <p className="text-main-500 flex-shrink-0 flex-grow-0 text-center text-5xl">
+                  {score?.totalScore}점
+                </p>
+              </div>
+              <p className="text-2xl text-black">입니다.</p>
             </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section className="flex flex-col gap-6 mt-6">
-        <SearchBar
-          placeholder="찾는 내 프로젝트를 입력해주세요."
-          onSearchChange={setSearchKeyword}
-        />
-        <div className="flex flex-wrap gap-4">
-          {projects?.map((project) => (
-            <ProjectPost
-              key={project.id}
-              {...project}
-            />
-          ))}
-        </div>
+        <section className="mt-[3.63rem]">
+          <div className="relative h-[445px] w-full overflow-hidden rounded-[20px] bg-[#dfeafe]">
+            <div className="flex items-center justify-between bg-[#dfeafe] px-[2.25rem] py-[1.5rem]">
+              <p className="text-2xl font-semibold text-[#385b97]">내 점수</p>
+              <Button
+                variant="active"
+                className="w-auto px-[22px] py-[13px] text-center text-lg font-semibold"
+              >
+                점수 수정
+              </Button>
+            </div>
 
-      </section>
+            <div className="flex h-full flex-col items-start justify-start overflow-y-scroll rounded-xl px-[2.25rem]">
+              <div className="flex w-full flex-shrink-0 flex-grow-0 flex-col items-start justify-start rounded-xl bg-white">
+                {scoresByCategory?.map((category) => (
+                  <article
+                    key={category.categoryType}
+                    className="flex flex-shrink-0 flex-grow-0 items-center justify-between self-stretch px-8 py-6"
+                  >
+                    <p className="text-center text-lg font-semibold text-[#68696c]">
+                      {category.categoryNames.koreanName}
+                    </p>
+                    <p className="text-center text-lg font-semibold text-[#68696c]">
+                      {category.recognizedScore}점
+                    </p>
+                  </article>
+                ))}
+              </div>
+            </div>
+          </div>
+        </section>
 
+        <section className="mt-6 flex flex-col gap-6">
+          <SearchBar
+            placeholder="찾는 내 프로젝트를 입력해주세요."
+            onSearchChange={setSearchKeyword}
+          />
+          <div className="flex flex-wrap gap-4">
+            {projects?.map((project) => (
+              <ProjectPost key={project.id} {...project} />
+            ))}
+          </div>
+        </section>
+      </div>
     </div>
-  )
+  );
 }
--- src/view/project-participation/ui/ProjectParticipationView/index.tsx
+++ src/view/project-participation/ui/ProjectParticipationView/index.tsx
@@ -1,10 +1,12 @@
-import ProjectParticipationForm from "@/widget/project-participation/ui/ProjectParticipationForm";
+import ProjectParticipationForm from '@/widget/project-participation/ui/ProjectParticipationForm';
 
 export default function ProjectParticipationView() {
   return (
-    <div className="flex flex-col items-start w-full">
-      <h1 className="text-main-700 text-titleMedium mb-9">프로젝트 참여</h1>
-      <ProjectParticipationForm />
+    <div className="flex w-full justify-center px-4 py-15.5">
+      <div className="flex w-full max-w-[600px] flex-col items-start">
+        <h1 className="text-main-700 text-titleMedium mb-9">프로젝트 참여</h1>
+        <ProjectParticipationForm />
+      </div>
     </div>
   );
 }
