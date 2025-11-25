b4a96279c5b9b3e3d32b21ed6fca3a69b81dd63f
Dino0204 <s24020@gsm.hs.kr>
2025-11-25 18:12:26 +0900

    feat: Suspense for useSearchParams

--- src/view/project-create/ui/ProjectCreateView/index.tsx
+++ src/view/project-create/ui/ProjectCreateView/index.tsx
@@ -1,10 +1,13 @@
 import ProjectCreateForm from '@/feature/project-create/ui';
+import { Suspense } from 'react';
 
 export default function ProjectCreateView() {
   return (
     <div className="flex w-full justify-center px-4 py-15.5">
       <div className="flex w-full max-w-[600px] flex-col items-start">
-        <ProjectCreateForm />
+        <Suspense fallback={<div>Loading...</div>}>
+          <ProjectCreateForm />
+        </Suspense>
       </div>
     </div>
   );
