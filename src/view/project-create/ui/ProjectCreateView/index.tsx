d018b86af116c10d6b892a49938a562738b7cc6d
Dino0204 <s24020@gsm.hs.kr>
2025-11-25 15:05:59 +0900

    move: title to form

--- src/view/project-create/ui/ProjectCreateView/index.tsx
+++ src/view/project-create/ui/ProjectCreateView/index.tsx
@@ -4,7 +4,6 @@ export default function ProjectCreateView() {
   return (
     <div className="flex w-full justify-center px-4 py-15.5">
       <div className="flex w-full max-w-[600px] flex-col items-start">
-        <h1 className="text-main-700 text-titleMedium mb-9">프로젝트 생성</h1>
         <ProjectCreateForm />
       </div>
     </div>
--- src/view/project-participation/ui/ProjectParticipationView/index.tsx
+++ src/view/project-participation/ui/ProjectParticipationView/index.tsx
@@ -4,7 +4,6 @@ export default function ProjectParticipationView() {
   return (
     <div className="flex w-full justify-center px-4 py-15.5">
       <div className="flex w-full max-w-[600px] flex-col items-start">
-        <h1 className="text-main-700 text-titleMedium mb-9">프로젝트 참여</h1>
         <ProjectParticipationForm />
       </div>
     </div>
