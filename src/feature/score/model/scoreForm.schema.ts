38fa1eaccb99f1b341d42e25dc36bb43b65753d6
Dino0204 <s24020@gsm.hs.kr>
2025-11-28 02:23:25 +0900

    feat: score schema

---
+++ src/feature/score/model/scoreForm.schema.ts
@@ -0,0 +1,24 @@
+import { z } from 'zod';
+
+export const ScoreFormSchema = z
+  .object({
+    scoreId: z.number().optional(),
+    categoryType: z.string().toLowerCase(),
+    value: z.union([z.string(), z.number()]),
+    fileId: z.number().nullable(),
+    evidenceType: z.enum(['EVIDENCE', 'FILE', 'UNREQUIRED']),
+  })
+  .refine(
+    (data) => {
+      if (data.evidenceType === 'FILE' && !data.fileId) {
+        return false;
+      }
+      return true;
+    },
+    {
+      message: '파일을 첨부해주세요.',
+      path: ['fileId'],
+    },
+  );
+
+export type ScoreFormValues = z.infer<typeof ScoreFormSchema>;
