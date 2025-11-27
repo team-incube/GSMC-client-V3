ff4fd114e9b308d900d5464b8579a126d26d2a12
Dino0204 <s24020@gsm.hs.kr>
2025-11-27 19:57:08 +0900

    fix: project-participation

--- src/view/project-participation/ui/ProjectParticipationView/index.tsx
+++ src/view/project-participation/ui/ProjectParticipationView/index.tsx
@@ -1,38 +1,38 @@
 'use client';
 
-import { useParams } from 'next/navigation';
+import { useSearchParams } from 'next/navigation';
 
 import { useGetDraftEvidence } from '@/entities/evidence/model/useGetDraftEvidence';
 import { useGetProjectMyScoreById } from '@/entities/project/model/useGetProjectMyScoreById';
-import ProjectParticipationForm from '@/feature/project-participation/ui';
+import EvidenceForm from '@/feature/evidence/ui';
 
 export default function ProjectParticipationView() {
-  const params = useParams();
-  const rawId = Array.isArray(params.id) ? params.id[0] : params.id;
-  const projectId = Number(rawId);
-
+  const projectId = Number(useSearchParams().get('projectId'));
   const { data: projectScoreEvidence } = useGetProjectMyScoreById({ projectId });
   const { data: draftEvidence } = useGetDraftEvidence();
 
-  const initialValues = draftEvidence
-    ? {
-      title: draftEvidence.title,
-      content: draftEvidence.content,
-      fileIds: draftEvidence.fileIds,
-    }
-    : projectScoreEvidence?.score.evidence
-      ? {
-        title: projectScoreEvidence.score.evidence.title,
-        content: projectScoreEvidence.score.evidence.content,
-        evidenceId: projectScoreEvidence.score.evidence.evidenceId,
-        files: projectScoreEvidence.score.evidence.files ?? undefined,
-      }
-      : undefined;
+  const initialData = draftEvidence ? {
+    projectId,
+    title: draftEvidence.title,
+    content: draftEvidence.content,
+    fileIds: draftEvidence.fileIds,
+  } : projectScoreEvidence?.score ? {
+    projectId,
+    evidenceId: projectScoreEvidence.score.evidence?.evidenceId,
+    scoreId: projectScoreEvidence.score.scoreId,
+    title: projectScoreEvidence.score.evidence?.title,
+    content: projectScoreEvidence.score.evidence?.content,
+    files: projectScoreEvidence.score.evidence?.files ?? undefined,
+  } : { projectId };
+
+  const mode = initialData.evidenceId ? 'edit' : 'create';
+  const showDelete = !!initialData.evidenceId;
 
   return (
     <div className="flex w-full justify-center px-4 py-15.5">
       <div className="flex w-full max-w-[600px] flex-col items-start">
-        <ProjectParticipationForm initialValues={initialValues} />
+        <h1 className="text-main-700 text-titleMedium mb-9">프로젝트 참여</h1>
+        <EvidenceForm mode={mode} initialData={initialData} actions={{ showDraft: true, showDelete }} />
       </div>
     </div>
   );
