cce6bd7212209f25fe24096331e110735a38be24
Dino0204 <s24020@gsm.hs.kr>
2025-12-08 01:19:13 +0900

    feat: alerts

--- src/widget/header/ui/index.tsx
+++ src/widget/header/ui/index.tsx
@@ -1,29 +1,43 @@
+"use client"
+
+import { useState } from 'react';
+
 import Link from 'next/link';
 
 import { signout } from '@/feature/google-auth/lib/signout';
 import Bell from '@/shared/asset/svg/Bell';
+import AlertsModal from '@/widget/main/ui/AlertsModal';
 
 import { HEADER_NAV } from '../config/navigation';
 
 export default function Header() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
   return (
-    <header className="flex h-[70px] w-full items-center justify-center border-b border-gray-100 bg-white sticky top-0 z-50">
-      <div className="flex w-150 flex-shrink-0 flex-grow-0 items-center justify-between px-3 text-center">
-        <Link href="/main" className="text-main-800 text-xl font-bold">
-          GSMC
-        </Link>
+    <>
+      <header className="flex h-[70px] w-full items-center justify-center border-b border-gray-100 bg-white sticky top-0 z-50">
+        <div className="flex w-150 flex-shrink-0 flex-grow-0 items-center justify-between px-3 text-center">
+          <Link href="/main" className="text-main-800 text-xl font-bold">
+            GSMC
+          </Link>
 
-        <nav className="flex items-center gap-8 text-sm">
-          {HEADER_NAV.map((item) => (
-            <Link key={item.path} className="text-gray-500" href={item.path}>
-              {item.label}
-            </Link>
-          ))}
-          <button type="button" className="font-semibold cursor-pointer text-gray-900" onClick={signout}>로그아웃</button>
-        </nav>
+          <nav className="flex items-center gap-8 text-sm">
+            {HEADER_NAV.map((item) => (
+              <Link key={item.path} className="text-gray-500" href={item.path}>
+                {item.label}
+              </Link>
+            ))}
+            <button type="button" className="font-semibold cursor-pointer text-gray-900" onClick={signout}>로그아웃</button>
+          </nav>
 
-        <Bell />
-      </div>
-    </header>
+          <Bell onClick={() => setIsModalOpen(!isModalOpen)} className="cursor-pointer" />
+        </div>
+      </header>
+      {isModalOpen ? <div className="fixed top-[70px] left-1/2 -translate-x-1/2 w-150 z-50 pointer-events-none">
+        <div className="pointer-events-auto">
+          <AlertsModal />
+        </div>
+      </div> : null}
+    </>
   );
 }
