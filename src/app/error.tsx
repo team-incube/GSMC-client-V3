3622eccaa1ed0f118e56f1c5f54cc4a37aea850b
Dino0204 <s24020@gsm.hs.kr>
2025-12-10 08:06:30 +0900

    chore: error message

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -12,7 +12,7 @@ export default function Error({
   return (
     <div className="flex flex-col items-center justify-center w-full h-screen gap-6 px-4">
       <div className="flex flex-col items-center gap-2 text-center">
-        <h2 className="text-3xl font-bold text-main-700">500</h2>
+        <h2 className="text-3xl font-bold text-main-700">오류가 발생했습니다</h2>
         <p className="text-lg text-gray-600">{error.name}</p>
         <p className="text-gray-500">{error.message}</p>
       </div>
