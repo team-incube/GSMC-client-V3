800c05cf23e953fccf3c4a8fc6beded831465f38
Dino0204 <s24020@gsm.hs.kr>
2025-12-23 01:27:01 +0900

    fix: nullable

--- src/feature/score/model/scoreForm.schema.ts
+++ src/feature/score/model/scoreForm.schema.ts
@@ -5,7 +5,7 @@ export const ScoreFormSchema = z
     scoreId: z.number().optional(),
     categoryType: z.string().toLowerCase().min(1, '카테고리 타입을 선택해주세요.'),
     value: z.union([z.string().min(1, '제목을 입력해주세요.'), z.number().min(1, '점수를 입력해주세요.')]),
-    fileId: z.number("파일을 첨부해주세요."),
+    fileId: z.number("파일을 첨부해주세요.").nullable(),
     evidenceType: z.enum(['EVIDENCE', 'FILE', 'UNREQUIRED']),
     toeicAcademy: z.boolean().optional(),
   })
