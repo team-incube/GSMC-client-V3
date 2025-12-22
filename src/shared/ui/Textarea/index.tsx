9da2271cd4c423cc57f38b79b6136afc72f69a3f
Dino0204 <s24020@gsm.hs.kr>
2025-12-22 20:41:33 +0900

    feat: description

--- src/shared/ui/Textarea/index.tsx
+++ src/shared/ui/Textarea/index.tsx
@@ -2,15 +2,17 @@ import type { TextareaHTMLAttributes } from 'react';
 
 interface TextareaProps {
   label?: string;
+  description?: string;
 }
 
 type Props = TextareaProps & TextareaHTMLAttributes<HTMLTextAreaElement>;
 
-export default function Textarea({ label = '내용', ...props }: Props) {
+export default function Textarea({ label = '내용', description = '', ...props }: Props) {
   return (
     <div>
-      <label className="text-body1 text-main-700" htmlFor={label}>
-        {label}
+      <label className="w-full flex justify-between text-body1 text-main-700" htmlFor={label}>
+        <span>{label}</span>
+        <span className="ml-2 text-sm text-gray-500">{description}</span>
       </label>
       <textarea
         className="w-full resize-none rounded-2xl border border-gray-300 p-[20px]"
