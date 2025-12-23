f3b807bfd03894aab33a5c3a89ac1ccf44067d4f
Dino0204 <s24020@gsm.hs.kr>
2025-12-24 08:58:39 +0900

    fix: optional chaining

--- src/shared/ui/ProjectPost/index.tsx
+++ src/shared/ui/ProjectPost/index.tsx
@@ -9,7 +9,7 @@ export default function ProjectPost({ ...project }: ProjectType) {
     <article className="flex flex-col w-[188px] h-fit rounded-[10px] border border-black/10 bg-white overflow-hidden">
       <Link href={`/project/${project.id}`}>
         <div className="w-full h-[138px] object-cover overflow-hidden">
-          <Image src={project.files[0].uri || replace_logo} alt="project_image" width={300} height={138} />
+          <Image src={project.files?.[0]?.uri || replace_logo} alt="project_image" width={300} height={138} />
         </div>
 
         <div className="flex flex-col items-start flex-1 px-3">
@@ -21,9 +21,7 @@ export default function ProjectPost({ ...project }: ProjectType) {
               {project.description}
             </p>
             <p className="text-base font-semibold text-left text-black/60 line-clamp-2">
-              {project.participants.some((participant) => participant.id === project.ownerId)
-                ? project.participants.find((participant) => participant.id === project.ownerId)?.name
-                : ""} 외 {project.participants.length - 1}명
+              {`${project.participants.find(p => p.id === project.ownerId)?.name}${project.participants.length > 1 ? ` 외 ${project.participants.length - 1}명` : ''}`}
             </p>
           </div>
         </div>
