67a526dd156ecfda4d87ad3b26b76a250edf2fb7
Dino0204 <s24020@gsm.hs.kr>
2025-12-24 08:36:44 +0900

    fix: SEO에 영향을 주지 않는 role 제거

--- src/shared/ui/ProjectPost/index.tsx
+++ src/shared/ui/ProjectPost/index.tsx
@@ -8,7 +8,7 @@ export default function ProjectPost({ ...project }: ProjectType) {
   return (
     <article className="flex flex-col w-[188px] h-fit rounded-[10px] border border-black/10 bg-white overflow-hidden">
       <Link href={`/project/${project.id}`}>
-        <div className="w-full h-[138px] object-cover overflow-hidden" role="img">
+        <div className="w-full h-[138px] object-cover overflow-hidden">
           <Image src={project.files[0].uri || replace_logo} alt="project_image" width={300} height={138} />
         </div>
 
