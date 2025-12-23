e464d2a05113035a6153979232434f8e02ce8140
snowykte0426 <snowykte0426@naver.com>
2025-12-23 13:54:56 +0900

    fix: Zod 타입검사 활용

--- src/feature/google-auth/model/SignupSchema.ts
+++ src/feature/google-auth/model/SignupSchema.ts
@@ -5,8 +5,8 @@ export const SignupSchema = z.object({
   studentNumber: z
     .number('학번은 숫자여야 합니다.')
     .int()
-    .refine((val) => !isNaN(val) && val !== 0, '학번을 입력해주세요.')
-    .refine((val) => val >= 1101 && val <= 3418, '학번은 4자리 숫자여야 합니다.'),
+    .min(1101, '학번은 1101 이상이어야 합니다.')
+    .max(3418, '학번은 3418 이하이어야 합니다.'),
 });
 
 export type SignupFormType = z.infer<typeof SignupSchema>;
