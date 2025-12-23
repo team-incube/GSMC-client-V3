54ec0fbd8ac40063449b7c293ec0588aae03eab3
snowykte0426 <snowykte0426@naver.com>
2025-12-23 13:47:48 +0900

    update: 회원가입 스키마 타입 검사 강화

--- src/feature/google-auth/model/SignupSchema.ts
+++ src/feature/google-auth/model/SignupSchema.ts
@@ -3,10 +3,10 @@ import z from 'zod';
 export const SignupSchema = z.object({
   name: z.string().regex(/^[가-힣]{2,10}$/, '이름은 2~10자의 한글만 입력 가능합니다.'),
   studentNumber: z
-    .number()
+    .number('학번은 숫자여야 합니다.')
     .int()
-    .min(1000, '학번은 4자리 숫자여야 합니다.')
-    .max(9999, '학번은 4자리 숫자여야 합니다.'),
+    .refine((val) => !isNaN(val) && val !== 0, '학번을 입력해주세요.')
+    .refine((val) => val >= 1101 && val <= 3418, '학번은 4자리 숫자여야 합니다.'),
 });
 
 export type SignupFormType = z.infer<typeof SignupSchema>;
