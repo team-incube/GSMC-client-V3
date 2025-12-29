a531ea7ee055427587db01e7bcac18d9866224a0
snowykte0426 <snowykte0426@naver.com>
2025-12-29 15:52:20 +0900

    update: 회원가입 시 학번 검증로직 조건식 단순화

--- src/feature/google-auth/model/SignupSchema.ts
+++ src/feature/google-auth/model/SignupSchema.ts
@@ -9,9 +9,7 @@ export const SignupSchema = z.object({
       const grade = Math.floor(val / 1000);
       const classNum = Math.floor((val % 1000) / 100);
       const studentNum = val % 100;
-      if (grade < 1 || grade > 3) return false;
-      if (studentNum < 1 || studentNum > 18) return false;
-      return grade >= 1 && grade <= 3 && classNum >= 1 && classNum <= 4;
+      return grade >= 1 && grade <= 3 && classNum >= 1 && classNum <= 4 && studentNum >= 1 && studentNum <= 18;
     }, '유효하지 않은 학번입니다.'),
 });
 
