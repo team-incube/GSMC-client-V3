6bbd4cf1349e76ea5d557147eed155198ce61cf2
Dino0204 <s24020@gsm.hs.kr>
2025-12-29 18:27:51 +0900

    fix: project post 이미지

--- src/shared/ui/ProjectPost/index.tsx
+++ src/shared/ui/ProjectPost/index.tsx
@@ -8,20 +8,24 @@ export default function ProjectPost({ ...project }: ProjectType) {
   return (
     <article className="flex flex-col w-[188px] h-fit rounded-[10px] border border-black/10 bg-white overflow-hidden">
       <Link href={`/project/${project.id}`}>
-        <div className="w-full h-[138px] object-cover overflow-hidden">
-          <Image src={project.files?.[0]?.uri || replace_logo} alt="project_image" width={300} height={138} />
+        <div className="relative w-full h-[138px] overflow-hidden">
+          <Image
+            src={project.files?.[0]?.uri || replace_logo}
+            alt="project_image"
+            fill
+            className="object-cover"
+          />
         </div>
-
         <div className="flex flex-col items-start flex-1 px-3">
           <div className="flex flex-col justify-center items-start gap-1 py-2">
-            <p className="text-base font-semibold text-left text-black">
+            <p className="text-base font-semibold text-left text-black line-clamp-1">
               {project.title}
             </p>
             <p className="text-base font-semibold text-left text-black/40 line-clamp-2">
               {project.description}
             </p>
             <p className="text-base font-semibold text-left text-black/60 line-clamp-2">
-              {`${project.participants.find(p => p.id === project.ownerId)?.name}${project.participants.length > 1 ? ` 외 ${project.participants.length - 1}명` : ''}`}
+              {`${project.participants.find(p => p.id === project.ownerId)?.name}${project.participants.length > 1 ? ` 외 ${project.participants.length - 1}` : ''}`}
             </p>
           </div>
         </div>
