e6f934d10a02102202b7d7382752cc57c26c079c
Dino0204 <s24020@gsm.hs.kr>
2026-01-03 13:37:18 +0900

    feat: 학급 & 학년 별 상위 백분위 수 표기

--- src/view/main/ui/MainView/index.tsx
+++ src/view/main/ui/MainView/index.tsx
@@ -11,6 +11,7 @@ import Button from '@/shared/ui/Button';
 import ProjectPost from '@/shared/ui/ProjectPost';
 import SearchBar from '@/shared/ui/SearchBar';
 import ScoreManagementModal from '@/widget/main/ui/ScoreManagementModal';
+import { useGetPercentScore } from '@/entities/score/model/useGetPercentScore';
 
 export default function MainView() {
   const [searchKeyword, setSearchKeyword] = useState('');
@@ -18,7 +19,8 @@ export default function MainView() {
   const { data: student } = useGetCurrentStudent();
   const totalScore = useGetCombinedTotalScore();
   const scoresByCategory = useGetCombinedScoresByCategory();
-
+  const { data: classPercentScore } = useGetPercentScore({ type: "class" });
+  const { data: gradePercentScore } = useGetPercentScore({ type: "grade" });
   const { data: allProjects } = useGetProjects();
   const { data: searchedProjects } = useGetProjectBySearch({
     title: searchKeyword,
@@ -33,24 +35,28 @@ export default function MainView() {
         {isModalOpen ? <ScoreManagementModal setIsModalOpen={setIsModalOpen} /> : null}
 
         {/* 점수 헤더 섹션 */}
-        <section className="flex w-full justify-start">
+        <section className="flex w-full">
           <div className="flex w-full flex-col gap-4 sm:gap-6 md:gap-[27px] font-semibold">
-            <div className="flex flex-wrap items-baseline gap-2 sm:gap-3">
-              <p className="text-main-700 text-center text-2xl sm:text-3xl md:text-4xl">{student?.name}</p>
+            <div className="flex flex-wrap justify-center items-baseline gap-2 sm:gap-3">
+              <p className="text-main-700 text-center text-2xl sm:text-3xl md:text-4xl">{student?.name ?? "사용자"}</p>
               <p className="text-left text-lg sm:text-xl md:text-2xl text-black">님의 인증제 점수는</p>
             </div>
-            <div className="flex flex-wrap items-baseline gap-2">
+            <div className="flex flex-wrap justify-center items-baseline gap-2">
               <div className="flex items-center justify-center gap-2.5 rounded-full bg-[#f3f3f3] px-6 sm:px-8 md:px-9 py-2 sm:py-2.5 md:py-3">
                 <p className="text-main-500 text-center text-3xl sm:text-4xl md:text-5xl">
-                  {totalScore.approved}점
+                  {totalScore.approved ?? 0}점
                 </p>
               </div>
               <p className="text-black/40 text-center text-3xl sm:text-4xl md:text-5xl">
                 /
               </p>
               <p className="text-black/40 text-center text-lg sm:text-xl">
-                {totalScore.expected}점
+                {totalScore.expected ?? 0}점
               </p>
+            </div>
+            <div className="flex flex-wrap justify-center items-baseline gap-2">
+              <p className='bg-[#f3f3f3] text-main-500 px-2 py-1 rounded-xl'>학급 상위 {Math.floor(classPercentScore?.topPercentile ?? 0)}%</p>
+              <p className='bg-[#f3f3f3] text-main-500 px-2 py-1 rounded-xl'>학년 상위 {Math.floor(gradePercentScore?.topPercentile ?? 0)}%</p>
             </div>
           </div>
         </section>
