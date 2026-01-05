a3188e69145bf58945166e12ed1ded6b77b57321
Dino0204 <s24020@gsm.hs.kr>
2026-01-06 07:49:22 +0900

    fix: query-client option

--- src/shared/lib/query-client.ts
+++ src/shared/lib/query-client.ts
@@ -4,7 +4,9 @@ function makeQueryClient() {
   return new QueryClient({
     defaultOptions: {
       queries: {
-        staleTime: 60 * 1000,
+        staleTime: 1000 * 60 * 5,
+        refetchOnWindowFocus: false,
+        retry: 1,
       },
     },
   });
