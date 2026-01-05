19079d9bdc144aa8be383042dc8a1be6f51603f2
Dino0204 <s24020@gsm.hs.kr>
2026-01-05 14:03:00 +0900

    feat: 도메인 별 queryOptions

---
+++ src/entities/project/api/queries.ts
@@ -0,0 +1,30 @@
+import { queryOptions } from '@tanstack/react-query';
+
+import { getProjectById } from './getProjectById';
+import { getProjectBySearch, GetProjectBySearchRequest } from './getProjectBySearch';
+import { getProjectMyScoreById } from './getProjectMyScoreById';
+import { getProjects } from './getProjects';
+
+export const projectQueries = {
+  all: () => ['project'] as const,
+  list: () =>
+    queryOptions({
+      queryKey: [...projectQueries.all(), 'list'],
+      queryFn: getProjects,
+    }),
+  search: (params: GetProjectBySearchRequest) =>
+    queryOptions({
+      queryKey: [...projectQueries.all(), 'search', params],
+      queryFn: () => getProjectBySearch(params),
+    }),
+  detail: (id: number) =>
+    queryOptions({
+      queryKey: [...projectQueries.all(), 'detail', id],
+      queryFn: () => getProjectById({ projectId: Number(id) }),
+    }),
+  myScore: (id: number) =>
+    queryOptions({
+      queryKey: [...projectQueries.all(), 'myScore', id],
+      queryFn: () => getProjectMyScoreById({ projectId: Number(id) }),
+    }),
+};
