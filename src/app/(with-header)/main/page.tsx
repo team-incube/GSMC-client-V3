82abb94490e3e314b8aae688119ee9709040b4d5
Dino0204 <s24020@gsm.hs.kr>
2026-01-06 10:19:17 +0900

    feat: project, score prefetch

--- src/app/(with-header)/main/page.tsx
+++ src/app/(with-header)/main/page.tsx
@@ -1,5 +1,7 @@
 import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
 
+import { projectQueries } from '@/entities/project/api/queries';
+import { scoreQueries } from '@/entities/score/api/queries';
 import { studentQueries } from '@/entities/student/api/queries';
 import { getQueryClient } from '@/shared/lib/query-client';
 import MainView from '@/view/main/ui/MainView';
@@ -7,7 +9,12 @@ import MainView from '@/view/main/ui/MainView';
 export default async function MainPage() {
   const queryClient = getQueryClient();
 
-  await queryClient.prefetchQuery(studentQueries.me());
+  Promise.all([
+    queryClient.prefetchQuery(studentQueries.me()),
+    queryClient.prefetchQuery(scoreQueries.category()),
+    queryClient.prefetchQuery(projectQueries.list()),
+  ])
+
 
   return (
     <HydrationBoundary state={dehydrate(queryClient)}>
