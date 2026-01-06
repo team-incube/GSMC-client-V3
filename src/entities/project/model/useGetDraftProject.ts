18b3d24c6e74c2535d634dbd898627955807f42b
Dino0204 <s24020@gsm.hs.kr>
2026-01-06 10:29:23 +0900

    fix: 임시 저장 프로젝트가 없을 때 타입 에러

--- src/entities/project/model/useGetDraftProject.ts
+++ src/entities/project/model/useGetDraftProject.ts
@@ -1,9 +1,10 @@
 import { useSuspenseQuery } from '@tanstack/react-query';
 
 import { getDraftProject } from '../api/getDraftProject';
+import { ProjectType } from '../model/project';
 
 export const useGetDraftProject = () => {
-  return useSuspenseQuery({
+  return useSuspenseQuery<ProjectType | null>({
     queryKey: ['project', 'draft'],
     queryFn: () => getDraftProject(),
     retry: false,
