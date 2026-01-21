826ed3da7dc96bdbefcd194d29cc7bde8764838b
Dino0204 <s24020@gsm.hs.kr>
2026-01-21 12:24:39 +0900

    fix: 삼항 연산자로 변경

--- src/entities/score/api/addScoreByCategoryType.ts
+++ src/entities/score/api/addScoreByCategoryType.ts
@@ -19,16 +19,8 @@ export const addScoreByCategoryType = async ({
   value,
   fileId,
 }: AddScoreByCategoryTypeRequest): Promise<ScoreType> => {
-  if (POST_CATEGORIES.includes(categoryType)) {
-    const response = await instance.post(`/scores/${categoryType}`, {
-      value,
-      fileId,
-    });
-    return response.data;
-  }
-  const response = await instance.put(`/scores/${categoryType}`, {
-    value,
-    fileId,
-  });
+  const response = POST_CATEGORIES.includes(categoryType)
+    ? await instance.post(`/scores/${categoryType}`, { value, fileId })
+    : await instance.put(`/scores/${categoryType}`, { value, fileId });
   return response.data;
 };
